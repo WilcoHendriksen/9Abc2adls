a3b9c6aadb470b18db6e1c88e4a04e1b4f031778
Wilco Hendriksen <wilco.hendriksen@9altitudes.com>
2023-08-30 10:02:22 +0200

    #50180 improve upload script

--- scripts/publish-file-to-storage.ps1
+++ scripts/publish-file-to-storage.ps1
@@ -9,28 +9,18 @@ param (
     [Parameter(Mandatory)] [string] $fileToUpload,
     # name of the blob
     [Parameter(Mandatory)] [string] $blobFileName
-    # set to not replace the latest version
-    # [switch] $skipLatest
 )
   # Some logging
-  #Write-Host "Publishing $version to storage account $storageAccountName, container $containerName."
+  Write-Host "Publishing $blobFileName to storage account $storageAccountName, container $storageContainer."
 
   try {
-    # Set metadata
-    # $BlobName = "$($blobPrefix)$Version/$fileNameOnBlob"
-    # $BlobNameLatest = "$($blobPrefix)latest/$fileNameOnBlob"
-
     # Create context
     $Ctx = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
 
     # Upload blob
     Write-Host "Uploading $blobFileName..."
     Set-AzureStorageBlobContent -Context $Ctx -Container $storageContainer -File $fileToUpload -Blob $blobFileName -Force
-
-    # if (!$skipLatest) {
-    #   # Upload latest blob
-    #   Set-AzureStorageBlobContent -Context $Ctx -Container $containerName -File $fileToUpload -Blob $BlobNameLatest -Metadata $Metadata -Force
-    # }
+    Write-Host "Publishing of $blobFileName succeeded."
   }
   catch {
     Write-Error "Could not publish blob with name $blobFileName, $_"
